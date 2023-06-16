7711749f46691ae54873dcbf662e73b37c8d65fe
ckinger <cartersk23@gmail.com>
2023-06-15 21:43:55 -0500

    Finish the blog and footer containers

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -7,7 +7,7 @@ const Article = ({ imgUrl, date, title }) => {
       <div className='gpt3__blog-container_article-image'>
         <img src={imgUrl} alt='blog'/>
       </div>
-      <div className='gpt3__blog-container_articl-content'>
+      <div className='gpt3__blog-container_article-content'>
         <div>
           <p>{ date }</p>
           <h3>{ title }</h3>
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -11,10 +11,10 @@ const Blog = () => {
 We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
-        <div className='gpt3__blog-container-groupA'>
+        <div className='gpt3__blog-container_groupA'>
           <Article imgUrl={blog01} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
         </div>
-        <div className='gpt3__blog-container-groupB'>
+        <div className='gpt3__blog-container_groupB'>
           <Article imgUrl={blog02} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
           <Article imgUrl={blog03} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
           <Article imgUrl={blog04} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
